c52ee97ebce0a166634c7752977a9160c049f6fe
blablack <blablack@5d164f6a-86a1-45f5-b308-b13062061d0d>
2012-12-28 15:08:35 +0000

    VCF Filter resonance bug fix
    git-svn-id: svn://svn.code.sf.net/p/avwlv2/code/trunk@112 5d164f6a-86a1-45f5-b308-b13062061d0d

--- src/vcf.cpp
+++ src/vcf.cpp
@@ -12,8 +12,8 @@ using namespace lvtk;
 #include "vcf.hpp"
 
 Vcf::Vcf(double rate):
-Plugin<Vcf>(p_n_ports)
-{
+		Plugin<Vcf>(p_n_ports)
+		{
 	synthdata = new SynthData(1);
 	//QString qs;
 
@@ -27,7 +27,7 @@ Plugin<Vcf>(p_n_ports)
 	fInvertRandMax= 1.0f/(float)RAND_MAX ;
 	b_noise = 19.1919191919191919191919191919191919191919;
 	pi2_rate = 2.0f * M_PI / rate; // how often changes the rate? I guess once on init, or?
-	inv2_rate = 2.0 / rate;// this double seems unnecessary
+	inv2_rate = 2.0 / (double)rate;// this double seems unnecessary
 
 	//port_M_in = new Port(tr("In"), PORT_IN, 0, this);
 	//port_M_freq = new Port(tr("Freq"), PORT_IN, 1, this);
@@ -54,7 +54,7 @@ Plugin<Vcf>(p_n_ports)
 	//configDialog->addSlider(tr("Lin. FM Gain"), vcfLinFMGain, 0, 10);
 	//configDialog->addSlider(tr("Resonance"), resonance, 0.01, 1);
 	//configDialog->addSlider(tr("Resonance Gain"), resonanceGain, 0, 1);
-}
+		}
 
 void Vcf::initBuf()
 {
@@ -117,300 +117,300 @@ void Vcf::run(uint32_t nframes)
 	vcfExpFMGain = *p(p_expFMGain);
 	vcfLinFMGain = *p(p_linFMGain);
 	resonance = *p(p_resonance);
-	resonanceGain = 0;
+	resonanceGain = *p(p_resonanceGain);
 	gain = *p(p_inputGain);
 	freq = *p(p_freq);
 	vcfType = floor(*p(p_vcfType));
 
 	switch (vcfType)
 	{
-	case VCF_LR:
-	{
-		double b_noiseout;
-		int i_noise;
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-
-		for (l2 = 0; l2 < nframes; ++l2)
+		case VCF_LR:
 		{
-			f = freq_const * (synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2]);
-			if (f < 0.0) f = 0.0;
-			else if (f > 0.99) f = 0.99;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			fa = 1.0 - f;
-			fb = q * (1.0 + (1.0 / fa));
-			// generate dither (?) noise
-			b_noise = b_noise * b_noise;
-			i_noise = (int)b_noise;
-			b_noise = b_noise - i_noise;
-			b_noiseout = b_noise - 1.5;// was 0.5
-			b_noise = b_noise + 19.0;
+			double b_noiseout;
+			int i_noise;
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
 
-			buf_lr[0] = fa * buf_lr[0] + f * (gain * inData[l2] + fb * (buf_lr[0] - buf_lr[1]) + 0.00001 * b_noiseout);
-			buf_lr[1] = fa * buf_lr[1] + f * buf_lr[0];
-			p(p_out)[l2] = buf_lr[1];
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = freq_const * (synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2]);
+				if (f < 0.0) f = 0.0;
+				else if (f > 0.99) f = 0.99;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				fa = 1.0 - f;
+				fb = q * (1.0 + (1.0 / fa));
+				// generate dither (?) noise
+				b_noise = b_noise * b_noise;
+				i_noise = (int)b_noise;
+				b_noise = b_noise - i_noise;
+				b_noiseout = b_noise - 1.5;// was 0.5
+				b_noise = b_noise + 19.0;
+
+				buf_lr[0] = fa * buf_lr[0] + f * (gain * inData[l2] + fb * (buf_lr[0] - buf_lr[1]) + 0.00001 * b_noiseout);
+				buf_lr[1] = fa * buf_lr[1] + f * buf_lr[0];
+				p(p_out)[l2] = buf_lr[1];
+			}
+			break;
 		}
-		break;
-	}
-	case VCF_LPF:
-	{
-		double temp;
-
-		q0 = resonance;
-
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
+		case VCF_LPF:
 		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ) f = MAX_FREQ;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			iv_sin = sin(pi2_rate * f);
-			iv_cos = cos(pi2_rate * f);
-			iv_alpha = iv_sin/(64.0 * q);
-			b0 = (1.0 - iv_cos) * 0.5;
-			b1 = 1.0 - iv_cos;
-			b2 = b0;
-			a0 = 1.0 + iv_alpha;
-			a1 = -2.0 * iv_cos;
-			a2 = 1.0 - iv_alpha;
-			temp = 1.0 / a0 * (b0 * gain * inData[l2] + b1 * buf_lpf[0] + b2 * buf_lpf[1] - a1 * buf_lpf[2] - a2 * buf_lpf[3]);
-			p(p_out)[l2]=temp;
-			buf_lpf[1] = buf_lpf[0];
-			buf_lpf[0] = gain * inData[l2];
-			buf_lpf[3] = buf_lpf[2];
-			buf_lpf[2] = temp;//data[0][l2];
-		}
-		break;
-	}
-	case VCF_HPF:
-	{
-		double temp;
+			double temp;
 
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
+			q0 = resonance;
+
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ) f = MAX_FREQ;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				iv_sin = sin(pi2_rate * f);
+				iv_cos = cos(pi2_rate * f);
+				iv_alpha = iv_sin/(64.0 * q);
+				b0 = (1.0 - iv_cos) * 0.5;
+				b1 = 1.0 - iv_cos;
+				b2 = b0;
+				a0 = 1.0 + iv_alpha;
+				a1 = -2.0 * iv_cos;
+				a2 = 1.0 - iv_alpha;
+				temp = 1.0 / a0 * (b0 * gain * inData[l2] + b1 * buf_lpf[0] + b2 * buf_lpf[1] - a1 * buf_lpf[2] - a2 * buf_lpf[3]);
+				p(p_out)[l2]=temp;
+				buf_lpf[1] = buf_lpf[0];
+				buf_lpf[0] = gain * inData[l2];
+				buf_lpf[3] = buf_lpf[2];
+				buf_lpf[2] = temp;//data[0][l2];
+			}
+			break;
+		}
+		case VCF_HPF:
 		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ) f = MAX_FREQ;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			iv_sin = sin(pi2_rate * f);
-			iv_cos = cos(pi2_rate * f);
-			iv_alpha = iv_sin/(64.0 * q);
-			b0 = (1.0 + iv_cos) * 0.5;
-			b1 = - 1.0 - iv_cos;
-			b2 = b0;
-			a0 = 1.0 + iv_alpha;
-			a1 = -2.0 * iv_cos;
-			a2 = 1.0 - iv_alpha;
-			temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_hpf[0] + b2 * buf_hpf[1] - a1 * buf_hpf[2] - a2 * buf_hpf[3]);
-			p(p_out)[l2]=temp;
-			buf_hpf[1] = buf_hpf[0];
-			buf_hpf[0] = gain * inData[l2];
-			buf_hpf[3] = buf_hpf[2];
-			buf_hpf[2] = temp;//data[0][l2];
-		}
-		break;
-	}
-	case VCF_BPF_I:
-	{
-		double temp;
+			double temp;
 
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ) f = MAX_FREQ;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				iv_sin = sin(pi2_rate * f);
+				iv_cos = cos(pi2_rate * f);
+				iv_alpha = iv_sin/(64.0 * q);
+				b0 = (1.0 + iv_cos) * 0.5;
+				b1 = - 1.0 - iv_cos;
+				b2 = b0;
+				a0 = 1.0 + iv_alpha;
+				a1 = -2.0 * iv_cos;
+				a2 = 1.0 - iv_alpha;
+				temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_hpf[0] + b2 * buf_hpf[1] - a1 * buf_hpf[2] - a2 * buf_hpf[3]);
+				p(p_out)[l2]=temp;
+				buf_hpf[1] = buf_hpf[0];
+				buf_hpf[0] = gain * inData[l2];
+				buf_hpf[3] = buf_hpf[2];
+				buf_hpf[2] = temp;//data[0][l2];
+			}
+			break;
+		}
+		case VCF_BPF_I:
 		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ) f = MAX_FREQ;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			iv_sin = sin(pi2_rate * f);
-			iv_cos = cos(pi2_rate * f);
-			iv_alpha = iv_sin/(64.0 * q);
-			b0 = q * iv_alpha;
-			b1 = 0.0;
-			b2 = -q * iv_alpha;
-			a0 = 1.0 + iv_alpha;
-			a1 = -2.0 * iv_cos;
-			a2 = 1.0 - iv_alpha;
+			double temp;
 
-			temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_bpf_i[0] + b2 * buf_bpf_i[1] - a1 * buf_bpf_i[2] - a2 * buf_bpf_i[3]);
-			p(p_out)[l2]=temp;
-			buf_bpf_i[1] = buf_bpf_i[0];
-			buf_bpf_i[0] = gain * inData[l2];
-			buf_bpf_i[3] = buf_bpf_i[2];
-			buf_bpf_i[2] = temp;//data[0][l2];
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ) f = MAX_FREQ;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				iv_sin = sin(pi2_rate * f);
+				iv_cos = cos(pi2_rate * f);
+				iv_alpha = iv_sin/(64.0 * q);
+				b0 = q * iv_alpha;
+				b1 = 0.0;
+				b2 = -q * iv_alpha;
+				a0 = 1.0 + iv_alpha;
+				a1 = -2.0 * iv_cos;
+				a2 = 1.0 - iv_alpha;
+
+				temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_bpf_i[0] + b2 * buf_bpf_i[1] - a1 * buf_bpf_i[2] - a2 * buf_bpf_i[3]);
+				p(p_out)[l2]=temp;
+				buf_bpf_i[1] = buf_bpf_i[0];
+				buf_bpf_i[0] = gain * inData[l2];
+				buf_bpf_i[3] = buf_bpf_i[2];
+				buf_bpf_i[2] = temp;//data[0][l2];
+			}
+			break;
 		}
-		break;
-	}
-	case VCF_BPF_II:
-	{
-		double temp;
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
+		case VCF_BPF_II:
 		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ) f = MAX_FREQ;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			iv_sin = sin(pi2_rate * f);
-			iv_cos = cos(pi2_rate * f);
-			iv_alpha = iv_sin/(64.0 * q);
-			b0 = iv_alpha;
-			b1 = 0.0;
-			b2 = -iv_alpha;
-			a0 = 1.0 + iv_alpha;
-			a1 = -2.0 * iv_cos;
-			a2 = 1.0 - iv_alpha;
-			temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_bpf_ii[0] + b2 * buf_bpf_ii[1] - a1 * buf_bpf_ii[2] - a2 * buf_bpf_ii[3]);
-			p(p_out)[l2] = temp;
-			buf_bpf_ii[1] = buf_bpf_ii[0];
-			buf_bpf_ii[0] = gain * inData[l2];
-			buf_bpf_ii[3] = buf_bpf_ii[2];
-			buf_bpf_ii[2] = temp;//data[0][l2];
+			double temp;
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ) f = MAX_FREQ;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				iv_sin = sin(pi2_rate * f);
+				iv_cos = cos(pi2_rate * f);
+				iv_alpha = iv_sin/(64.0 * q);
+				b0 = iv_alpha;
+				b1 = 0.0;
+				b2 = -iv_alpha;
+				a0 = 1.0 + iv_alpha;
+				a1 = -2.0 * iv_cos;
+				a2 = 1.0 - iv_alpha;
+				temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_bpf_ii[0] + b2 * buf_bpf_ii[1] - a1 * buf_bpf_ii[2] - a2 * buf_bpf_ii[3]);
+				p(p_out)[l2] = temp;
+				buf_bpf_ii[1] = buf_bpf_ii[0];
+				buf_bpf_ii[0] = gain * inData[l2];
+				buf_bpf_ii[3] = buf_bpf_ii[2];
+				buf_bpf_ii[2] = temp;//data[0][l2];
+			}
+			break;
 		}
-		break;
-	}
-	case VCF_NF:
-	{
-		double temp;
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
+		case VCF_NF:
 		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ) f = MAX_FREQ;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			iv_sin = sin(pi2_rate * f);
-			iv_cos = cos(pi2_rate * f);
-			iv_alpha = iv_sin/(64.0 * q);
-			b0 = 1.0;
-			b1 = -2.0 * iv_cos;
-			b2 = 1.0;
-			a0 = 1.0 + iv_alpha;
-			a1 = -2.0 * iv_cos;
-			a2 = 1.0 - iv_alpha;
+			double temp;
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ) f = MAX_FREQ;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				iv_sin = sin(pi2_rate * f);
+				iv_cos = cos(pi2_rate * f);
+				iv_alpha = iv_sin/(64.0 * q);
+				b0 = 1.0;
+				b1 = -2.0 * iv_cos;
+				b2 = 1.0;
+				a0 = 1.0 + iv_alpha;
+				a1 = -2.0 * iv_cos;
+				a2 = 1.0 - iv_alpha;
 
-			temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_nf[0] + b2 * buf_nf[1] - a1 * buf_nf[2] - a2 * buf_nf[3]);
-			p(p_out)[l2] = temp;// conversion
-			buf_nf[1] = buf_nf[0];
-			buf_nf[0] = gain * inData[l2];
-			buf_nf[3] = buf_nf[2];
-			buf_nf[2] = temp;//data[0][l2];
+				temp = 1.0 / a0 * (gain * b0 * inData[l2] + b1 * buf_nf[0] + b2 * buf_nf[1] - a1 * buf_nf[2] - a2 * buf_nf[3]);
+				p(p_out)[l2] = temp;// conversion
+				buf_nf[1] = buf_nf[0];
+				buf_nf[0] = gain * inData[l2];
+				buf_nf[3] = buf_nf[2];
+				buf_nf[2] = temp;//data[0][l2];
+			}
+			break;
 		}
-		break;
-	}
-	case VCF_MOOG1:
-	{// Timo Tossavainen version
-		double b_noiseout;
-		int i_noise;
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
+		case VCF_MOOG1:
+		{// Timo Tossavainen version
+			double b_noiseout;
+			int i_noise;
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ2) f = MAX_FREQ2;
+				q = q0 + resonanceGain * resonanceData[l2];
+				if (q < 0.01) q = 0.01;
+				else if (q > 1.0) q = 1.0;
+				fa = inv2_rate * f;
+				moog_f = fa * 1.16f;
+				revMoog = 1.0f - moog_f;
+				moog2times= moog_f * moog_f;
+				fb = 4.1 * q * (1.0 - 0.15 * moog2times);
+				// generate dither (?) noise
+				b_noise = b_noise * b_noise;
+				i_noise = (int)b_noise;
+				b_noise = b_noise - i_noise;
+
+				b_noiseout = b_noise - 1.5; // was - 0.5 now with - 1.0
+
+				b_noise = b_noise + 19.0;
+				in_moog1[0] = gain * inData[l2] + 0.000001 * b_noiseout;//((float)rand() * fInvertRandMax - 1.0);
+				in_moog1[0] -= fb * buf_moog1[4];
+				in_moog1[0] *=0.35013 * (moog2times * moog2times);
+				buf_moog1[1] = in_moog1[0] + 0.3 * in_moog1[1] + revMoog * buf_moog1[1];
+				in_moog1[1] = in_moog1[0];
+				buf_moog1[2] = buf_moog1[1] + 0.3 * in_moog1[2] + revMoog * buf_moog1[2];
+				in_moog1[2] = buf_moog1[1];
+				buf_moog1[3] = buf_moog1[2] + 0.3 * in_moog1[3] + revMoog * buf_moog1[3];
+				in_moog1[3] = buf_moog1[2];
+				buf_moog1[4] = buf_moog1[3] + 0.3 * in_moog1[4] + revMoog * buf_moog1[4];
+				in_moog1[4] = buf_moog1[3];
+				p(p_out)[l2] = buf_moog1[4];
+			}
+			break;
+		}
+		case VCF_MOOG2:                       // Paul Kellet version
 		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ2) f = MAX_FREQ2;
-			q = q0 + resonanceGain * resonanceData[l2];
-			if (q < 0.01) q = 0.01;
-			else if (q > 1.0) q = 1.0;
-			fa = inv2_rate * f;
-			moog_f = fa * 1.16f;
-			revMoog = 1.0f - moog_f;
-			moog2times= moog_f * moog_f;
-			fb = 4.1 * q * (1.0 - 0.15 * moog2times);
-			// generate dither (?) noise
-			b_noise = b_noise * b_noise;
-			i_noise = (int)b_noise;
-			b_noise = b_noise - i_noise;
+			double b_noiseout;
+			int i_noise;
 
-			b_noiseout = b_noise - 1.5; // was - 0.5 now with - 1.0
+			q0 = resonance;
+			freq_tune = 5.0313842f + freq;
+			gain_linfm = 1000.0f * vcfLinFMGain;
+			for (l2 = 0; l2 < nframes; ++l2)
+			{
+				f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
+				if (f < MIN_FREQ) f = MIN_FREQ;
+				else if (f > MAX_FREQ2) f = MAX_FREQ2;
+				qr = q0 + resonanceGain * resonanceData[l2];
+				if (qr < 0.01) qr = 0.01;
+				else if (qr > 1.0) qr = 1.0;
+				fb = inv2_rate * f;
+				q = 1.0 - fb;
+				pbis = fb + 0.8 * fb * q;
+				fa = pbis + pbis - 1.0;
+				q = qr * (1.0 + 0.5 * q * (1.0 - q + 5.6 * q * q));
+				// generate dither (?) noise
+				b_noise = b_noise * b_noise;
+				i_noise = (int)b_noise;
+				b_noise = b_noise - i_noise;
 
-			b_noise = b_noise + 19.0;
-			in_moog1[0] = gain * inData[l2] + 0.000001 * b_noiseout;//((float)rand() * fInvertRandMax - 1.0);
-			in_moog1[0] -= fb * buf_moog1[4];
-			in_moog1[0] *=0.35013 * (moog2times * moog2times);
-			buf_moog1[1] = in_moog1[0] + 0.3 * in_moog1[1] + revMoog * buf_moog1[1];
-			in_moog1[1] = in_moog1[0];
-			buf_moog1[2] = buf_moog1[1] + 0.3 * in_moog1[2] + revMoog * buf_moog1[2];
-			in_moog1[2] = buf_moog1[1];
-			buf_moog1[3] = buf_moog1[2] + 0.3 * in_moog1[3] + revMoog * buf_moog1[3];
-			in_moog1[3] = buf_moog1[2];
-			buf_moog1[4] = buf_moog1[3] + 0.3 * in_moog1[4] + revMoog * buf_moog1[4];
-			in_moog1[4] = buf_moog1[3];
-			p(p_out)[l2] = buf_moog1[4];
+				b_noiseout = b_noise - 1.5;// was 0.5
+
+				b_noise = b_noise + 19.0;
+
+				in_moog2[0] = gain * inData[l2] + 0.000001 * b_noiseout;//* ((float)rand() * fInvertRandMax - 1.0);
+				in_moog2[0] -= q * buf_moog2[4];
+				if (in_moog2[0] < -1.0) in_moog2[0] = -1.0;
+				if (in_moog2[0] > 1.0) in_moog2[0] = 1.0;
+				t1 = buf_moog2[1];
+				buf_moog2[1] = (in_moog2[0] + buf_moog2[0]) * pbis - buf_moog2[1] * fa;
+				t2 = buf_moog2[2];
+				buf_moog2[2] = (buf_moog2[1] + t1) * pbis - buf_moog2[2] * fa;
+				t1 = buf_moog2[3];
+				buf_moog2[3] = (buf_moog2[2] + t2) * pbis - buf_moog2[3] * fa;
+				buf_moog2[4] = (buf_moog2[3] + t1) * pbis - buf_moog2[4] * fa;
+				buf_moog2[4] -= buf_moog2[4] * buf_moog2[4] * buf_moog2[4] * 0.166667;
+				buf_moog2[0] = in_moog2[0];
+				p(p_out)[l2] = buf_moog2[4];
+			}
+			break;
 		}
-		break;
-	}
-	case VCF_MOOG2:                       // Paul Kellet version
-	{
-		double b_noiseout;
-		int i_noise;
-
-		q0 = resonance;
-		freq_tune = 5.0313842f + freq;
-		gain_linfm = 1000.0f * vcfLinFMGain;
-		for (l2 = 0; l2 < nframes; ++l2)
-		{
-			f = synthdata->exp2_table(freq_tune + freqData[l2] + vcfExpFMGain * expFMData[l2]) + gain_linfm * linFMData[l2];
-			if (f < MIN_FREQ) f = MIN_FREQ;
-			else if (f > MAX_FREQ2) f = MAX_FREQ2;
-			qr = q0 + resonanceGain * resonanceData[l2];
-			if (qr < 0.01) qr = 0.01;
-			else if (qr > 1.0) qr = 1.0;
-			fb = inv2_rate * f;
-			q = 1.0 - fb;
-			pbis = fb + 0.8 * fb * q;
-			fa = pbis + pbis - 1.0;
-			q = qr * (1.0 + 0.5 * q * (1.0 - q + 5.6 * q * q));
-			// generate dither (?) noise
-			b_noise = b_noise * b_noise;
-			i_noise = (int)b_noise;
-			b_noise = b_noise - i_noise;
-
-			b_noiseout = b_noise - 1.5;// was 0.5
-
-			b_noise = b_noise + 19.0;
-
-			in_moog2[0] = gain * inData[l2] + 0.000001 * b_noiseout;//* ((float)rand() * fInvertRandMax - 1.0);
-			in_moog2[0] -= q * buf_moog2[4];
-			if (in_moog2[0] < -1.0) in_moog2[0] = -1.0;
-			if (in_moog2[0] > 1.0) in_moog2[0] = 1.0;
-			t1 = buf_moog2[1];
-			buf_moog2[1] = (in_moog2[0] + buf_moog2[0]) * pbis - buf_moog2[1] * fa;
-			t2 = buf_moog2[2];
-			buf_moog2[2] = (buf_moog2[1] + t1) * pbis - buf_moog2[2] * fa;
-			t1 = buf_moog2[3];
-			buf_moog2[3] = (buf_moog2[2] + t2) * pbis - buf_moog2[3] * fa;
-			buf_moog2[4] = (buf_moog2[3] + t1) * pbis - buf_moog2[4] * fa;
-			buf_moog2[4] -= buf_moog2[4] * buf_moog2[4] * buf_moog2[4] * 0.166667;
-			buf_moog2[0] = in_moog2[0];
-			p(p_out)[l2] = buf_moog2[4];
-		}
-		break;
-	}
 	}
 }
 
--- wscript
+++ wscript
@@ -7,7 +7,7 @@ from waflib.extras import autowaf as autowaf
 
 # Variables for 'waf dist'
 APPNAME = 'avw.lv2'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # Mandatory variables
 top = '.'
